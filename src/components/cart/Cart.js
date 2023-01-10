520b1b2f8c0fcf8b2b99ab662de1e4ecec43eafa
elizaveta <liza.gold.omsk@gmail.com>
2023-01-10 08:17:38 +0300

    +filter +reg +sign in +cart data

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,11 +2,21 @@ import React, {useContext, useState} from 'react';
 import s from "./Cart.module.sass";
 import {Context} from "../../index";
 import {observer} from "mobx-react-lite";
+import {login, registration} from "../../http/userApi";
 
 const Cart = observer(() => {
     let {cart} = useContext(Context)
 
     const [num, setNum] = useState(1)
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [firstnameDirty, setFirstnameDirty] = useState(false)
+    const [nameDirty, setNameDirty] = useState(false)
+    const [phoneDirty, setPhoneDirty] = useState(false)
+    const [emailError, setEmailError] = useState("E-mail не может быть пустым")
+    const [nameError, setNameError] = useState("Имя не может быть пустым")
+    const [firstnameError, setFirstnameError] = useState("Фамилия не может быть пуста")
+    const [phoneError, setPhoneError] = useState("Телефон не может быть пустым")
+    const [regError, setRegError] = useState("")
     const [regField , setRegField] = useState({
         name: '',
         firstname: '',
@@ -14,17 +24,98 @@ const Cart = observer(() => {
         phone: '',
     })
     const regHandler = (e) =>{
-        setRegField( regField => ({...regField, [e.target.name]: e.target.value}))
+        if(e.target.name !== 'phone'){
+            setRegField( regField => ({...regField, [e.target.name]: e.target.value}))
+        }
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        // const tel = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+        if(e.target.name === 'email' && !re.test(String(e.target.value).toLowerCase())){
+            setEmailError('Некорректный E-mail')
+        }else{
+            setEmailError('')
+        }
+        if(e.target.name === 'name' && e.target.value.length < 2){
+            setNameError('Некорректное имя')
+        }else{
+            setNameError('')
+        }
+        if(e.target.name === 'firstname' && e.target.value.length < 8 && e.target.value.length > 0){
+            setFirstnameError("Некорректная фамилия")
+        }else{
+            setFirstnameError('')
+        }
+        if(e.target.name === 'phone'){
+            e.target.value = e.target.value.replace(/\D/g, "")
+            let inputValue =""
+            if(["7", "8", "9"].indexOf(e.target.value[0]) > -1){
+                if(e.target.value[0] === "9"){
+                    e.target.value = "7" + e.target.value
+                }
+                let firstSymbols = (e.target.value[0] === "8") ? "8" : "+7"
+                inputValue = firstSymbols + " "
+                if(e.target.value.length > 1){
+                    inputValue += "(" + e.target.value.substring(1, 4)
+
+                }
+                if(e.target.value.length >= 5){
+                    inputValue += ") " + e.target.value.substring(4, 7)
+                }
+                if(e.target.value.length >= 8){
+                    inputValue += "-" + e.target.value.substring(7, 9)
+                }
+                if(e.target.value.length >= 10){
+                    inputValue += "-" + e.target.value.substring(9, 11)
+                }
+            }else{
+                inputValue = "+" + e.target.value
+            }
+            e.target.value = inputValue
+            console.log(e.target.value)
+            console.log(e.target.value.length)
+            setRegField( regField => ({...regField, [e.target.name]: e.target.value}))
+            setPhoneError('')
+        }
+    }
+    const blurHandler = (e) => {
+        switch (e.target.name){
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'firstname':
+                setFirstnameDirty(true)
+                break
+            case 'name':
+                setNameDirty(true)
+                break
+            case 'phone':
+                setPhoneDirty(true)
+                break
+            default:
+                break
+        }
     }
 
-    const regButton = () =>{
-        // setRegField(false)
-        console.log(regField)
-        cart.setActive(false)
-        //вставить отправку данных
-        //вставить очистку полей
-        //вставить автоматическую авторизацию
-    }
+    // const regAuth = async () => {
+    //     let data
+    //     try {
+    //         if(tab == false){
+    //             data = await login(authField.email,authField.password)
+    //             console.log(data)
+    //         }else{
+    //             data = await registration(regField.email, regField.phone, regField.name, regField.password)
+    //             console.log(data)
+    //         }
+    //         user.setIsUser(user)
+    //         user.setIsAuth(true)
+    //
+    //     } catch (e){
+    //         if(tab == false) {
+    //             setAuthError(e.response.data.message)
+    //         }else{
+    //             setRegError(e.response.data.message)
+    //         }
+    //     }
+    // }
     return(
 
         <div className={cart.isActive ? `${s.root} ${s.active}` : `${s.root}`} onClick={() => cart.setActive(false)}>
@@ -58,11 +149,16 @@ const Cart = observer(() => {
                 </div>
                 <div className={s.root_reg}>
                     <form action="" className={s.root__form}>
-                        <input type="text" className={s.root__form_name} placeholder="Ваше имя" onChange={regHandler} name="name"/>
-                        <input type="text" className={s.root__form_name} placeholder="Ваша фамилия" onChange={regHandler} name="firstname"/>
-                        <input type="email" className={s.root__form_email} placeholder="Ваш E-mail" onChange={regHandler} name="email"/>
-                        <input type="tel" className={s.root__form_phone} placeholder="+7 (999) 999-99-99" onChange={regHandler} name="phone"/>
-                        <p onClick={regButton} className={s.root__btn_submit}>Оплатить</p>
+                        <input type="text" className={s.root__form_name} placeholder="Ваше имя" onChange={regHandler} onBlur={e => blurHandler(e)} name="name"/>
+                        {(nameDirty && nameError) && <div>{nameError}</div>}
+                        <input type="text" className={s.root__form_password} placeholder="Ваша фамилия" onChange={regHandler} onBlur={e => blurHandler(e)} name="firstname"/>
+                        {(firstnameDirty && firstnameError) && <div>{firstnameError}</div>}
+                        <input type="email" className={s.root__form_email} placeholder="Ваш E-mail" onChange={regHandler} onBlur={e => blurHandler(e)} name="email"/>
+                        {(emailDirty && emailError) && <div>{emailError}</div>}
+                        <input type="tel" className={s.root__form_phone} placeholder="+7 (999) 999-99-99" maxLength="18" onChange={regHandler} onBlur={e => blurHandler(e)} name="phone"/>
+                        {(phoneDirty && phoneError) && <div>{phoneError}</div>}
+                        <div>{regError}</div>
+                        <p className={s.root__btn_submit}>Зарегестрироваться</p>
                     </form>
                 </div>
             </div>
